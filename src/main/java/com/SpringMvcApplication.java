47582b1ca0b3f2d8f16a67b9c355c37f0a19bd21
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-11-08 22:13:12 +0100

    düzeldi sukur

--- src/main/java/com/SpringMvcApplication.java
+++ src/main/java/com/SpringMvcApplication.java
@@ -1,19 +1,15 @@
-package com.spring_mvc;
-
+package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 //@ComponentScan ek açıklaması, Spring tarafından taranacak üç paketi (ve alt paketleri) belirtmek için basePackages özniteliğini kullanır
 @ComponentScan({"controller"}) //run class ı sonuc class ıyla farklı package de ve
-//aynı hizada old için çalışabilmesi için bunu ekledik, aynı package de olsalar buna gerek yoktu
-
+//aynı hizada old için çalışabilmesi için bunu ekledik, aynı package de olsalar buna gerek yoktu.
 public class SpringMvcApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
 }
-//runner class
+//runnerclass
--- src/main/java/controller/SonucController.java
+++ src/main/java/controller/SonucController.java
@@ -1,4 +1,4 @@
-package com.spring_mvc.controller;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
